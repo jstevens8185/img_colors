4b13b0bb7400051352f273b0ff6043e05246ae49
Jered Stevens <j.stevens8185@gmail.com>
2023-10-09 15:34:18 -0500

    print output to seperate file instead of console

--- img_colors.go
+++ img_colors.go
@@ -8,12 +8,12 @@ import (
 	"os"
 )
 
-// PrintImagePixels prints the RGB values of the pixels in the provided image.
-// It takes a filename as input and returns an error if there are any issues.
-func PrintImagePixels(filename string) error {
-	reader, err := os.Open(filename)
+// PrintImagePixelsToFile prints the RGB values of the pixels in the provided image to a file.
+// It takes a filename for the output file and a filename for the input image as input and returns an error if there are any issues.
+func PrintImagePixelsToFile(inputFilename, outputFilename string) error {
+	reader, err := os.Open(inputFilename)
 	if err != nil {
-		return fmt.Errorf("failed to open file: %v", err)
+		return fmt.Errorf("failed to open input file: %v", err)
 	}
 	defer reader.Close()
 
@@ -22,6 +22,12 @@ func PrintImagePixels(filename string) error {
 		return fmt.Errorf("failed to decode image: %v", err)
 	}
 
+	outputFile, err := os.Create(outputFilename)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %v", err)
+	}
+	defer outputFile.Close()
+
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
 
@@ -29,7 +35,8 @@ func PrintImagePixels(filename string) error {
 		for x := 0; x < width; x++ {
 			color := img.At(x, y)
 			r, g, b, _ := color.RGBA()
-			fmt.Printf("Pixel at (%d, %d) - R: %d, G: %d, B: %d\n", x, y, r>>8, g>>8, b>>8)
+			// Write the output to the file instead of printing to the console.
+			fmt.Fprintf(outputFile, "Pixel at (%d, %d) - R: %d, G: %d, B: %d\n", x, y, r>>8, g>>8, b>>8)
 		}
 	}
 
